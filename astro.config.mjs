18085a2911e5657643e905c5b636d689ea5ba807
Nelson Boralli Neto <netoboralli@gmail.com>
2024-12-13 13:11:19 -0300

    Adding netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  // base: 'page/1'
+
   redirects: {
     '/': '/page/1',
-  }
+  },
+
+  adapter: netlify()
 });
