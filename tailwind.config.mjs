0040e67317ea83e9afe9457c113ac537a76693cd
Nelson Boralli Neto <netoboralli@gmail.com>
2024-12-13 12:02:51 -0300

    Adding Type Label

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,34 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			normal: '#aaaa99',
+			fire: '#ff4422',
+			water: '#3399ff',
+			electric: '#ffcc33',
+			grass: '#77cc55',
+			ice: '#66ccff',
+			fighting: '#bb5544',
+			poison: '#aa5599',
+			ground: '#ddbb55',
+			flying: '#8899ff',
+			psychic: '#ff5599',
+			bug: '#aabb22',
+			rock: '#bbaa66',
+			ghost: '#6666bb',
+			dragon: '#7766ee',
+			dark: '#775544',
+			steel: '#aaaabb',
+			fairy: '#ee99ee',
+			white: '#fff',
+			black: '#000'
+		},
+		fontFamily: {
+			'fira': ["Fira Sans", 'sans-serif']
+		}
 	},
 	plugins: [],
+	corePlugins: {
+		preflight: false,
+	}
 }
